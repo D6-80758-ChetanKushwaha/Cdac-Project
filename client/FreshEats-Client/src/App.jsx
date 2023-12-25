07326fe81e834c81ab24c08fe676f88022c0f6cd
D6-80428-pradeep <pradeepsinghsatna1998@gmail.com>
2023-12-25 19:16:21 +0530

    Login UI | Navigation Bar | Footer completed ....

--- client/FreshEats-Client/src/App.jsx
+++ client/FreshEats-Client/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Example from './Components/Header'
+import Login from './Components/Login'
+// import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './Components/Footer'
+import Dashboard from './Components/Dashboard'
+import Cart from './Components/Cart'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <Login/> */}
+      <Router>
+          {/* <Route path='/' element={<Example/>} /> */}
+        <Example/>
+
+        <Routes>
+          <Route path="" element={<Dashboard/>}/>
+          <Route path='/login' element={<Login/>} />
+          <Route path='/Cart' element={<Cart/>}/>
+        </Routes>
+
+        <Footer/>
+      </Router>
+
     </>
   )
 }
---
+++ client/FreshEats-Client/src/Components/Cart.jsx
@@ -0,0 +1,9 @@
+function Cart() {
+    return (
+        <div style={{fontSize: "larger"}}>
+            Cart
+        </div>
+    )
+}
+
+export default Cart
