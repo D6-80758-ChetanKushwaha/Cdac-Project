2f05fe67e0618f12dcbda71b54a012e743e3c839
D6-80428-pradeep <pradeepsinghsatna1998@gmail.com>
2024-02-06 18:05:08 +0530

    Cart Add Functionality Working

--- client/FreshEats-Client/src/Components/Cart.jsx
+++ client/FreshEats-Client/src/Components/Cart.jsx
@@ -16,6 +16,13 @@ const Cart = () => {
         setQuantity(quantity + 1);
     };
 
+    function getCartProd(){
+        const dataList = localStorage.getItem("myCartProds")
+        return dataList ? JSON.parse(dataList) : null;
+    }
+    const dataList = getCartProd()
+    console.log(dataList)
+
     return (
         <div className="bg-gray-100 h-screen py-8">
             <div className="container mx-auto px-4">
@@ -30,28 +37,47 @@ const Cart = () => {
                                         <th className="text-left font-semibold">Price</th>
                                         <th className="text-left font-semibold">Quantity</th>
                                         <th className="text-left font-semibold">Total</th>
+                                        <th className="text-left font-semibold"></th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td className="py-4">
-                                            <div className="flex items-center">
-                                                <img className="h-16 w-16 mr-4" src="https://componentland.com/images/7ObHe1EuFqmsZmri6-ROD.png" alt="Product image" />
-                                                <span className="font-semibold">Product name</span>
-                                            </div>
-                                        </td>
-                                        <td className="py-4">&#8377; {productPrice.toFixed(2)}</td>
-                                        <td className="py-4">
-                                            <div className="flex items-center">
-                                                <button className="border rounded-md py-2 px-4 mr-2" onClick={handleDecrease}>-</button>
-                                                <span className="text-center w-8">{quantity}</span>
-                                                <button className="border rounded-md py-2 px-4 ml-2" onClick={handleIncrease}>+</button>
-                                            </div>
-                                        </td>
-                                        <td className="py-4">&#8377; {(productPrice * quantity).toFixed(2)}</td>
-                                    </tr>
+                                    
+                                {
+                                    dataList.map((obj,index)=>(
+                                        <tr>
+                                            <td className="py-4">
+                                                <div className="flex items-center">
+                                                    <img className="h-16 w-16 mr-4" src={obj["image-url"]} alt="Product image" />
+                                                    <span className="font-semibold">{obj.title}</span>
+                                                </div>
+                                            </td>
+                                            <td className="py-4">&#8377; {obj.price.toFixed(2)}</td>
+                                            <td className="py-4">
+                                                <div className="flex items-center">
+                                                    <button className="border rounded-md py-2 px-4 mr-2" onClick={handleDecrease}>-</button>
+                                                    <span className="text-center w-8">{quantity}</span>
+                                                    <button className="border rounded-md py-2 px-4 ml-2" onClick={handleIncrease}>+</button>
+                                                </div>
+                                            </td>
 
-                                    <tr>
+                                            <td className="py-4">&#8377; {(obj.price * quantity).toFixed(2)}</td>
+                                            <td >
+
+                                                <svg style={{"cursor":"pointer"}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+    </svg>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }   
+                                    
+                            
+                                
+                        
+                                    
+                                    
+                                   
+                                    {/* <tr>
                                         <td className="py-4">
                                             <div className="flex items-center">
                                                 <img className="h-16 w-16 mr-4" src="https://componentland.com/images/neZnfwBHi0f-4TivjA6BS.png" alt="Product image" />
@@ -67,7 +93,7 @@ const Cart = () => {
                                             </div>
                                         </td>
                                         <td className="py-4">&#8377; {(productPrice * quantity).toFixed(2)}</td>
-                                    </tr>
+                                    </tr> */}
                                     {/* More product rows */}
                                 </tbody>
                             </table>
